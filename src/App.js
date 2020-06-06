9ff1fdbfbb86a982fe32dc689fd66b52a22ddb36
Ana Sacueda <asauceda@broadway.com>
2020-06-06 08:36:03 -0400

    removes more boilerplate CRA code, sets grid layout and frame layout in CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Story from './components/Story';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Game />
+      <Story />
     </div>
   );
 }
